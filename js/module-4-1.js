a48d24ffc3953119ab761d609a693b0cbe1350a6
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-04-22 16:01:18 +0300

    создает модуль 4-1 и 4-2

---
+++ js/module-4-1.js
@@ -0,0 +1,7 @@
+const a = 'Переменная "a" в module-4-1.js';
+
+console.log(a);
+
+const b = 'Переменная "b" в module-4-1.js';
+
+console.log(b);
---
+++ js/module-4-2.js
@@ -0,0 +1,7 @@
+const a = 'Переменная "a" в module-4-2.js';
+
+console.log(a);
+
+const c = 'Переменная "c" в module-4-2.js';
+
+console.log(c);
