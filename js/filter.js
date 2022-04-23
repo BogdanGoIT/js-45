6bd3fac5a69189695d9443803de08d282c0684fa
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-04-24 00:00:25 +0300

    делает filter

--- js/filter.js
+++ js/filter.js
@@ -14,17 +14,29 @@ const players = [
     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
-    { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+    { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
 ];
 
-const filteredNumberes = numbers.filter(number => number > 20);
-
-// console.log(filteredNumberes);
-
-// когда в параметрах ф-ции ({свойство}) при деструктуризации
-const onlinePlayers = players.filter( ({online}) => online );
-console.table(onlinePlayers);
 
 // когда просто {свойство} при деструктуризации
 const { a, b } = { a: 5, b: 10 };
 console.log(a, b);
+
+
+
+const filteredNumberes = numbers.filter(number => number > 20);
+// console.log(filteredNumberes);
+
+// получаем массив всех онлайн игроков
+const onlinePlayers = players.filter( player => player.online );
+console.table(onlinePlayers);
+
+// получаем массив всех офлайн игроков
+// когда в параметрах ф-ции ({свойство}) при деструктуризации
+const offlinePlayers = players.filter(({ online }) => !online);
+console.table(offlinePlayers);
+
+// получаем список хардкорных игроков с временем больше 250
+const hardcorePlayer = players.filter(({ timePlayed }) => timePlayed > 250);
+console.table(hardcorePlayer);
+
