9e0bac132610c42adbde7b4a1f2112d4eee5368d
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-04-24 13:30:31 +0300

    хотя бы один онлайн - some

--- js/every-some.js
+++ js/every-some.js
@@ -1,7 +1,7 @@
 const players = [
-    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: true },
-    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
-    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: false },
+    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: false },
     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
     { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
 ];
@@ -11,4 +11,11 @@ const players = [
 // - возвращает true если все элементы удовлетворюют условию
 
 const isAllOnline = players.every(player => player.online);
-console.log(isAllOnline);
+// console.log(isAllOnline);
+
+// Array.prototype.some()
+// - поэлиментно перебирает оригинальный массив
+// - возвращает true если хотя бы один элимент массива удовлитворяет условию
+
+const isAnyOnline = players.some(({ online }) => online)
+console.log(isAnyOnline);
