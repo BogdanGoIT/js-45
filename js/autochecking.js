6c0e62e6427c041a1d372d4c4fdeb26d7dcb7be3
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-04-27 20:11:40 +0300

    работа filterMap и indexOf

--- js/autochecking.js
+++ js/autochecking.js
@@ -1,43 +1,148 @@
 
-const books = [
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     genres: ["adventure", "history"],
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     genres: ["fiction", "mysticism"],
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     genres: ["horror", "mysticism", "adventure"],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(book => book.genres);
+// console.log(allGenres);
+// const uniqueGenres = allGenres.filter((course, index, array) => array.indexOf(course) === index);
+// console.log(uniqueGenres);
+
+// function showThis() {
+//   console.log("this in showThis: ", this);
+// }
+
+// // Викликаємо у глобальному контексті
+// showThis(); // this in showThis: Window
+
+// const user = {
+//   username: "Mango",
+// };
+
+// // Записуємо посилання на функцію у властивість об'єкта
+// // Зверніть увагу, що це не виклик - немає ()
+// user.showContext = showThis;
+
+// // Викликаємо функцію в контексті об'єкта
+// // this буде вказувати на поточний об'єкт, в контексті
+// // якого здійснюється виклик, а не на глобальний об'єкт.
+// user.showContext(); // this in showThis: {username: "Mango", showContext: ƒ}
+
+
+// const getFriends = (users) => users.flatMap(user => user.friends):
+// ['Sharron Pace', 'Briana Decker', 'Sharron Pace', 'Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner', 'Goldie Gentry', 'Aisha Tran', 'Jordan Sampson', 'Eddie Strong', 'Jacklyn Lucas', 'Linda Chapman', 'Goldie Gentry', 'Briana Decker']
+
+// .filter((friends, index, array) => array.indexOf(friends) === index):
+
+// friends = Sharron Pace
+// index = 0
+// array = ['Sharron Pace', 'Briana Decker', 'Sharron Pace', 'Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner', 'Goldie Gentry', 'Aisha Tran', 'Jordan Sampson', 'Eddie Strong', 'Jacklyn Lucas', 'Linda Chapman', 'Goldie Gentry', 'Briana Decker']
+
+// array.indexOf(Sharron Pace) === 0
+// 0 === 0 // true
+// [Sharron Pace, ]
+
+// friends = Briana Decker
+// index = 1
+// array = ['Sharron Pace', 'Briana Decker', 'Sharron Pace', 'Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner', 'Goldie Gentry', 'Aisha Tran', 'Jordan Sampson', 'Eddie Strong', 'Jacklyn Lucas', 'Linda Chapman', 'Goldie Gentry', 'Briana Decker']
+// array.indexOf(Briana Decker) === 1
+// 1 === 1 // true
+// [Sharron Pace, Briana Decker, ]
+
+// friends = Sharron Pace
+// index = 2
+// array = ['Sharron Pace', 'Briana Decker', 'Sharron Pace', 'Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner', 'Goldie Gentry', 'Aisha Tran', 'Jordan Sampson', 'Eddie Strong', 'Jacklyn Lucas', 'Linda Chapman', 'Goldie Gentry', 'Briana Decker']
+// array.indexOf(Sharron Pace) === 2
+// 0 === 2 // false
+// [Sharron Pace, Briana Decker, ]
+
+const getFriends = (users) => users.flatMap(user => user.friends).filter((friends, index, array) => array.indexOf(friends) === index);
+
+
+
+console.log(getFriends([
   {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    genres: ["adventure", "history"],
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
   },
   {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    genres: ["fiction", "mysticism"],
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
   },
   {
-    title: "Redder Than Blood",
-    author: "Tanith Lee",
-    genres: ["horror", "mysticism", "adventure"],
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
   },
-];
-// Change code below this line
-const allGenres = books.flatMap(book => book.genres);
-console.log(allGenres);
-const uniqueGenres = allGenres.filter((course, index, array) => array.indexOf(course) === index);
-console.log(uniqueGenres);
-
-function showThis() {
-  console.log("this in showThis: ", this);
-}
-
-// Викликаємо у глобальному контексті
-showThis(); // this in showThis: Window
-
-const user = {
-  username: "Mango",
-};
-
-// Записуємо посилання на функцію у властивість об'єкта
-// Зверніть увагу, що це не виклик - немає ()
-user.showContext = showThis;
-
-// Викликаємо функцію в контексті об'єкта
-// this буде вказувати на поточний об'єкт, в контексті
-// якого здійснюється виклик, а не на глобальний об'єкт.
-user.showContext(); // this in showThis: {username: "Mango", showContext: ƒ}
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+]));
