b831582f9fe91cb3a9a135a6a2a8d8301083901b
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-04-28 16:38:02 +0300

    делает конструктор машин

--- js/constructors.js
+++ js/constructors.js
@@ -1,24 +1,89 @@
 // Основы ООП: класс, экземпляр (обьект), интерфейс
 
 
-// Функции - конструкторы
-// - именование
-// - оператор new
-// - cвойство Function.prototype
+// // деструктуризируем в параметрах:
+// const Car = function ({ brand, model, price } = {}) {
+//     // деструктуризация аргументов в теле:
+//     // const { brand, model, price } = config;
 
-// const Car = function (value) {
 //     // 2. Функция вызываеться в контексте созданого обьекта
 //     //    тоесть в this записываеться ссылка на него
-//     console.log(this);
+     
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
 
-//     this.a = value;
+//     // 3. в свойство this.[[Prototype]] записываеться ссылка на обьект Car.prototype
+//     //    тоесть Car.prototype это ПРОТОТИП будущего обьекта (экземпляра)
+    
 //     // 4. ссылка на обьект возвращаеться в место вызова new Car
-// }
+// };
+
+
+// Car.prototype.changePrice = function (newPrice) {
+//          price = newPrice;
+//  }
 
 
 // // 1. Если функция вызывается через new, создаеться пустой обьект
-// const myCar = new Car(5);
+// const myCar = new Car({
+//     brand: 'Audi',
+//     model: 'b6',
+//     price: '350000'
+// });
 // console.log(myCar);
 
-// const myCar2 = new Car(10);
+// const myCar2 = new Car({
+//     brand: 'BMW',
+//     model: 'x5',
+//     price: '200000'
+// });
+
 // console.log(myCar2);
+
+// const myCar3 = new Car();
+// console.log(myCar3);
+
+
+// деструктуризируем в параметрах:
+const Car = function ({ brand, model, price } = {}) {
+    // деструктуризация аргументов в теле:
+    // const { brand, model, price } = config;
+
+    // 2. Функция вызываеться в контексте созданого обьекта
+    //    тоесть в this записываеться ссылка на него
+     
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+
+    // 3. в свойство this.[[Prototype]] записываеться ссылка на обьект Car.prototype
+    //    тоесть Car.prototype это ПРОТОТИП будущего обьекта (экземпляра)
+    
+    // 4. ссылка на обьект возвращаеться в место вызова new Car
+};
+
+
+Car.prototype.changePrice = function (newPrice) {
+         price = newPrice;
+ }
+
+
+// 1. Если функция вызывается через new, создаеться пустой обьект
+const myCar = new Car({
+    brand: 'Audi',
+    model: 'b6',
+    price: '350000'
+});
+console.log(myCar);
+
+const myCar2 = new Car({
+    brand: 'BMW',
+    model: 'x5',
+    price: '200000'
+});
+
+console.log(myCar2);
+
+const myCar3 = new Car();
+console.log(myCar3);
